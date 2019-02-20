96e671c7868035dd03eca54f8739d100d4847ed7
bonnw <pwonghansa@gmail.com>
2019-02-19 20:55:33 -0600

    initialized project with IntelliJ

---
+++ src/main/java/com/burahan/restorders/RestordersApplication.java
@@ -0,0 +1,15 @@
+package com.burahan.restorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestordersApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RestordersApplication.class, args);
+    }
+
+}
